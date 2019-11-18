6585396cf51e3e60350b5feb86107165d241eb64
David Rodríguez Fernández <drf8@alu.ua.es>
2019-11-18 22:01:47 +0100

    Inicio de state

--- include/state.h
+++ include/state.h
@@ -1,4 +1,15 @@
 #ifndef STATE_H
 #define STATE_H
   #include <iostream>
+  #include <vector>
+
+  using namespace std;
+  class State{
+    private:
+      int estado[][];
+
+    public:
+      State(int size);
+      int getCelda(int i, int j);
+  };
 #endif
